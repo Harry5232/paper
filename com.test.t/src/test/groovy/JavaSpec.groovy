675af7813deb5c14d902d6f0d9efa61e89a01124
Harry5232 <harry5232@gmail.com>
2018-04-15 14:19:47 +0800

    0415完成版

--- com.test.t/src/test/groovy/JavaSpec.groovy
+++ com.test.t/src/test/groovy/JavaSpec.groovy
@@ -8,13 +8,14 @@ import com.test.t3.Scores
   
  //"DIFFICULT" type   
  @Scores(points=2.0f)
- @Unroll
+ //@Unroll
  def "integrationOfSquare"() {
-    setup:
+    given:
             com.test.t.HW1Solution ob = new com.test.t.HW1Solution();
     expect:
             ob.integrationOfSquare(a,b,c) == d
-          where:   
+            
+    where:   
               a    | b    | c    ||  d
          	  20.0 | 35.0 | 10   || (double)12986.111111111117
               60.0 | 35.0 | 10   || (double)-64441.87242798354
@@ -26,19 +27,20 @@ import com.test.t3.Scores
   
  //"DIFFICULT" type   
  @Scores(points=7.0f)
- @Unroll
+ //@Unroll
  def "reverse"() {
-    setup:
+    given:
             com.test.t.HW1Solution ob = new com.test.t.HW1Solution();
     expect:
             ob.reverse(a) == c
-          where:   
+            
+    where:   
               a  ||  c
          	  10 || (int)1
-              42 || (int)24
-              32 || (int)23
-              34 || (int)43
-              42 || (int)24
+              3  || (int)3
+              36 || (int)63
+              20 || (int)20
+              50 || (int)5
                  	     
   }
  
@@ -46,31 +48,14 @@ import com.test.t3.Scores
   
  //"DIFFICULT" type   
  @Scores(points=2.0f)
- @Unroll
- def "isArmstrongNumber"() {
-    setup:
-            com.test.t.HW1Solution ob = new com.test.t.HW1Solution();
-    expect:
-            ob.isArmstrongNumber(a) == c
-          where:   
-              a  ||  c
-         	  20 || (boolean)false
-              60 || (boolean)false
-              80 || (boolean)false
-                 	     
-  }
- 
-
-  
- //"DIFFICULT" type   
- @Scores(points=2.0f)
- @Unroll
+ //@Unroll
  def "findAllArmstrongNumbers"() {
-    setup:
+    given:
             com.test.t.HW1Solution ob = new com.test.t.HW1Solution();
     expect:
             ob.findAllArmstrongNumbers(a,b) == c
-          where:   
+            
+    where:   
               a   | b   ||  c
          	  200 | 400 || "370,371"
               100 | 999 || "153,370,371,407"
@@ -80,17 +65,37 @@ import com.test.t3.Scores
  
 
   
+ //"DIFFICULT" type   
+ @Scores(points=2.0f)
+ //@Unroll
+ def "isArmstrongNumber"() {
+    given:
+            com.test.t.HW1Solution ob = new com.test.t.HW1Solution();
+    expect:
+            ob.isArmstrongNumber(a) == c
+            
+    where:   
+              a  ||  c
+         	  20 || (boolean)false
+              60 || (boolean)false
+              80 || (boolean)false
+                 	     
+  }
+ 
+
+  
  //"EASY" type   
  @Scores(points=5.0f)
- @Unroll
+ //@Unroll
  def "diamond"() {
-    setup:
+    given:
             com.test.t.HW1Solution ob = new com.test.t.HW1Solution();
     expect:
             ob.diamond(a) == c
-          where:   
+            
+    where:   
               a ||  c
-         	  4 || "---4---\n--333--\n-22222-\n1111111\n-22222-\n--333--\n---4---\n"
+         	  4 || "---4---\n--0333--\n-22222-\n1111111\n-22222-\n--333--\n---4---\n"
               3 || "--3--\n-222-\n11111\n-222-\n--3--\n"
               6 || "-----6-----\n----555----\n---44444---\n--3333333--\n-222222222-\n11111111111\n-222222222-\n--3333333--\n---44444---\n----555----\n-----6-----\n"
                  	     
@@ -98,15 +103,35 @@ import com.test.t3.Scores
  
 
   
+ //"DIFFICULT" type   
+ @Scores(points=5.0f)
+ //@Unroll
+ def "numOfFactors"() {
+    given:
+            com.test.t.HW1Solution ob = new com.test.t.HW1Solution();
+    expect:
+            ob.numOfFactors(a,b,c) == d
+            
+    where:   
+              a  | b  | c  ||  d
+         	  10 | 20 | 20 || (int)0
+              13 | 27 | 7  || (int)0
+              38 | 49 | 29 || (int)0
+                 	     
+  }
+ 
+
+  
  //"HARD" type   
  @Scores(points=9.0f)
- @Unroll
+ //@Unroll
  def "numberOfBitOne"() {
-    setup:
+    given:
             com.test.t.HW1Solution ob = new com.test.t.HW1Solution();
     expect:
             ob.numberOfBitOne(a) == c
-          where:   
+            
+    where:   
               a  ||  c
          	  20 || (int)2
               60 || (int)4
@@ -116,33 +141,16 @@ import com.test.t3.Scores
  
 
   
- //"DIFFICULT" type   
- @Scores(points=5.0f)
- @Unroll
- def "numOfFactors"() {
-    setup:
-            com.test.t.HW1Solution ob = new com.test.t.HW1Solution();
-    expect:
-            ob.numOfFactors(a,b,c) == d
-          where:   
-              a  | b  | c  ||  d
-         	  10 | 20 | 20 || (int)0
-              14 | 12 | 46 || (int)1
-              42 | 4  | 43 || (int)5
-                 	     
-  }
- 
-
-  
  //"EASY" type   
  @Scores(points=4.0f)
- @Unroll
+ //@Unroll
  def "numberOfBitChanges"() {
-    setup:
+    given:
             com.test.t.HW1Solution ob = new com.test.t.HW1Solution();
     expect:
             ob.numberOfBitChanges(a) == c
-          where:   
+            
+    where:   
               a  ||  c
          	  20 || (int)4
               35 || (int)3
